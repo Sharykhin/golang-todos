019536a9c0c72c382ed89a751a9f44c2ff50cd8b
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-02-05 12:34:48 +0300

    some thinks

--- main.go
+++ main.go
@@ -11,16 +11,18 @@ import (
 )
 
 func main() {
+	//TODO: is it okay that we can permamment connection to sqlite database?
+	db, err := sql.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	// TODO: routes can be moved to somewhere else I guess
 	http.Handle("/", middleware.Logger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//TODO: Dependencies should be resolved somewhere else I guess
-		db, err := sql.New()
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer db.Close()
 		sqlRepository := sql.TodoRepository{db}
 		handler.Index(w, r, sqlRepository)
 	})))
+	// TODO: this one might be taken from env variable.
 	http.ListenAndServe("localhost:8082", nil)
 }
