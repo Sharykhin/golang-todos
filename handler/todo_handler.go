22da72bac991021319ab31c6446a84d876f67f12
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-02-06 11:34:45 +0300

    move repositories and count to separate goroutine

--- handler/todo_handler.go
+++ handler/todo_handler.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
 	"time"
 
+	"github.com/sharykhin/todoapp/entity"
 	"github.com/sharykhin/todoapp/repository/todo"
 	"github.com/sharykhin/todoapp/request"
 )
@@ -16,18 +18,60 @@ type TodoHandler struct {
 func (th TodoHandler) Index(w http.ResponseWriter, r *http.Request, repository todo.Repositier) {
 	limit := th.Handler.queryParam(r, "limit", "10")
 	offset := th.Handler.queryParam(r, "offset", "0")
-	//TODO: so we have two functions that should be run in a separate goroutines
-	todos, err := repository.Get(limit, offset)
 
-	if err != nil {
-		th.Handler.serverError(w, err)
-		return
-	}
+	chTodos := make(chan []entity.Todo)
+	chCount := make(chan *int)
+	chErr := make(chan error)
+	done := make(chan bool)
 
-	count, err := repository.Count()
-	if err != nil {
-		th.Handler.serverError(w, err)
-		return
+	var todos []entity.Todo
+	var count *int
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func(chTodos chan<- []entity.Todo, chErr chan<- error, wg *sync.WaitGroup) {
+		todos, err := repository.Get(limit, offset)
+		if err != nil {
+			chErr <- err
+		}
+		chTodos <- todos
+		wg.Done()
+	}(chTodos, chErr, &wg)
+
+	go func(chCount chan<- *int, chErr chan<- error, wg *sync.WaitGroup) {
+		count, err := repository.Count()
+		if err != nil {
+			chErr <- err
+		}
+		chCount <- count
+		wg.Done()
+	}(chCount, chErr, &wg)
+
+	go func(wg *sync.WaitGroup) {
+		wg.Wait()
+		done <- true
+	}(&wg)
+
+	for {
+		completed := false
+		select {
+		case t := <-chTodos:
+			todos = t
+		case c := <-chCount:
+			count = c
+		case err := <-chErr:
+			th.Handler.serverError(w, err)
+			return
+		case <-done:
+			close(chTodos)
+			close(chCount)
+			close(chErr)
+			completed = true
+			break
+		}
+		if completed == true {
+			break
+		}
 	}
 
 	th.Handler.success(w, todos, map[string]interface{}{"total": count, "count": len(todos)})
