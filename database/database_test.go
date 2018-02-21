8b1f399d6fe021f858c7e863775d5e10e386ac74
mac-222 <mac-222@mac-222s-Mac-mini.local>
2018-02-21 11:20:25 +0300

    add tets on error

--- database/database_test.go
+++ database/database_test.go
@@ -1,72 +1,75 @@
 package database
 
 import (
-	//"testing"
-	"github.com/stretchr/testify/mock"
 	"context"
-	//"database/sql"
-	//"github.com/stretchr/testify/assert"
 	"testing"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	"github.com/Sharykhin/golang-todos/entity"
 	"github.com/stretchr/testify/assert"
+	"github.com/pkg/errors"
 )
 
-type mockDb struct {
-	mock.Mock
-}
-
-func (m mockDb) QueryRowContext(ctx context.Context, query string) *mockRow {
-	ret := m.Called(ctx, query)
-	return ret.Get(0).(*mockRow)
-}
-
-type mockRow struct {
-	mock.Mock
-}
-
-func(m mockRow) Scan(dest ...interface{}) error {
-	ret := m.Called(dest)
-	err := ret.Get(0)
-	if err != nil {
-		return err.(error)
-	}
-	return nil
-
-}
-
 func TestCreate(t *testing.T) {
-	var err error
-	var mockS sqlmock.Sqlmock
-	db, mockS, err = sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+	t.Run("success creation", func (t *testing.T) {
+		var err error
+		var mockS sqlmock.Sqlmock
+		db, mockS, err = sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		defer db.Close()
 
-	ctx := context.Background()
-	cc, _ := context.WithCancel(ctx)
-	rt := entity.CreateParams{
-		Title: "test title",
-		Description: "test description",
-		Completed: false,
-	}
+		ctx := context.Background()
+		cc, _ := context.WithCancel(ctx)
+		rt := entity.CreateParams{
+			Title: "test title",
+			Description: "test description",
+			Completed: false,
+		}
 
-	mockS.ExpectExec("INSERT INTO todos").
-		WithArgs("test title", "test description", false).
-		WillReturnResult(sqlmock.NewResult(1,1))
+		mockS.ExpectExec("INSERT INTO todos").
+			WithArgs(rt.Title, rt.Description, rt.Completed).
+			WillReturnResult(sqlmock.NewResult(1,1))
 
+		nt, err := Create(cc, rt)
+		if err != nil {
+			t.Errorf("error was not expected while inserting a new row: %v", err)
+		}
+		if err := mockS.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %v", err)
+		}
+		assert.Equal(t, "test title", nt.Title)
+		assert.Equal(t, "test description", nt.Description)
+	})
 
-	nt, err := Create(cc, rt)
-	if err != nil {
-		t.Errorf("error was not expected while inserting a new row: %v", err)
-	}
-	if err := mockS.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %v", err)
-	}
-	assert.Equal(t, "test title", nt.Title)
-	assert.Equal(t, "test description", nt.Description)
+	t.Run("error inserting", func(t *testing.T) {
+		var err error
+		var mockS sqlmock.Sqlmock
+		db, mockS, err = sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		defer db.Close()
 
+		ctx := context.Background()
+		cc, _ := context.WithCancel(ctx)
+		rt := entity.CreateParams{
+			Title: "test title",
+			Description: "test description",
+			Completed: false,
+		}
+
+		mockS.ExpectExec("INSERT INTO todos").
+			WithArgs(rt.Title, rt.Description, rt.Completed).
+			WillReturnResult(sqlmock.NewErrorResult(errors.New("could not insert row")))
+
+		nt, err := Create(cc, rt)
+		if err := mockS.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %v", err)
+		}
+		assert.Nil(t, nt)
+		assert.Equal(t, "could not get last insert id: could not insert row", err.Error())
+	})
 }
 
 //func TestCount(t *testing.T) {
